9a4425b0daec031bafb2f6aa2b4ad0d7cdd6e7c3
52DREAMSKY <805652806@qq.com>
2019-12-09 18:11:03 +0800

    完成了权限理管理部分代码，剩余角色管理代码待开发

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/users/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Goodslist from '../components/goods/Goodslist.vue'
 
 const routes = [
 	{path:'/',redirect:'/login'},
@@ -20,6 +21,7 @@ const routes = [
 			{path:"/users",component:Users},
 			{path:"/rights",component:Rights},
 			{path:"/roles",component:Roles},
+			{path:"/goods",component:Goodslist},
 		]
 	},
 ]
